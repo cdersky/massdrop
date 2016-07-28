b2a3b0a34fc8876e829dc2ec665a699c0f616236
claire <cdersky@gmail.com>
2016-07-28 08:45:35 -0700

    modularize getData file

--- getData.js
+++ getData.js
@@ -12,34 +12,27 @@ function getData(url, id){
       }); 
 
       res.on("end", function(){
-        SiteData.findOneAndUpdate({ // update entry with info
-          _id : id
-          } , { $set : {data: siteHTML , downloadComplete: true}},
-          {upsert: true},
-          function(err, data){
-            if(err){
-              console.log('error saving data ', err);
-            } else {
-              console.log('data saved');
-            }
-          }
-        ); 
+        saveDataToDB(id, siteHTML);
       });
     } else { // if status is not 200
-      SiteData.findOneAndUpdate({ // update entry with message
-        _id : id
-        } , { $set : {data: 'info was not retrievable' , downloadComplete: true}},
-        {upsert: true},
-        function(err, data){
-          if(err){
-            console.log('error saving data: ', err);
-          } else {
-            console.log('data saved');
-          }
-        }
-      ); 
+      saveDataToDB(id, 'info was not retrievable');
     } 
   });
+}
+
+function saveDataToDB (id, saveToDataField){
+  SiteData.findOneAndUpdate({ // update entry with info
+    _id : id
+    } , { $set : {data: saveToDataField , downloadComplete: true}},
+    {upsert: true},
+  function(err, data){
+    if(err){
+      console.log('error saving data ', err);
+    } else {
+      console.log('data saved');
+    }
+  }
+  ); 
 }
 
 module.exports = getData;
